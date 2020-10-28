76d626be0f520e5522e56fe8bf96c619bcdb8c5a
Nikos Chantzopoulos <nikoschantzopoulos@gmail.com>
2020-10-29 00:59:14 +0200

    Added various validations

--- src/views/Materials.js
+++ src/views/Materials.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react'
 import {
-    Content, Header, Form, FormGroup, ControlLabel, Breadcrumb,
-    FormControl, ButtonToolbar, Button, Panel, Icon, Steps, FlexboxGrid,  Row, Col
+    Message, Content, Header, Form, FormGroup, ControlLabel, Breadcrumb,
+    FormControl, ButtonToolbar, Button, Panel, Icon, Steps, FlexboxGrid, Row, Col
 } from 'rsuite'
 import { mandatory } from '../style/Style'
 
 const MaterialTab = () => {
-
-    /**
-     * Hanlde steps
-     */
-    const [step, setStep] = React.useState(0);
-    const onChange = nextStep => {
-        setStep(nextStep < 0 ? 0 : nextStep > 3 ? 3 : nextStep);
-    };
-    const onNext = () => onChange(step + 1);
-    const onPrevious = () => onChange(step - 1);
 
     /**
      * Define mandatory fields
@@ -25,7 +15,8 @@ const MaterialTab = () => {
     const [isKindMandatory, setIsKindMandatory] = useState(0)
     const [isDescriptionMandatory, setDescriptionMandatory] = useState(0)
     const [isQuantityMandatory, setQuantityMandatory] = useState(0)
-    const [isNextDisabled, setNextButtonDisabled] = useState()
+    const [isNextDisabled, setNextButtonDisabled] = useState(true)
+    const [hasAllMandatoryFields, setAllMandatoryFields] = useState(true)
 
 
     const [aa, setAA] = useState(0)
@@ -34,66 +25,79 @@ const MaterialTab = () => {
     const [description, setDescription] = useState('')
     const [quantity, setQuantity] = useState('')
     const [section, setSection] = useState('')
-    const [name,setName] = useState('')
+    const [name, setName] = useState('')
     const [restDetails, setRestDetails] = useState('')
     const [sector, setSector] = useState('')
 
-    
+/**
+ * Hanlde steps
+ */
+    const [step, setStep] = React.useState(0);
 
-    
+    const onChange = nextStep => {
+        setStep(nextStep < 0 ? 0 : nextStep > 3 ? 3 : nextStep);
+    };
+    const onNext = () => {
+        if (aa === 0 || code === "" || kind === "" || description === "" || quantity === "") {
+            setAllMandatoryFields(false)
+        }
+        else {
+            onChange(step + 1);
+        }
+    }
+
+    const onPrevious = () => onChange(step - 1);
+    const resetError = () => {
+        setAllMandatoryFields(true)
+    }
+
+
 
     /**
      *  Handle events
      */
     const handleAABlur = (e1) => {
-        console.log("blur"+ e1.target.value)
-        if (aa == null || aa === 0 ) {
+        console.log("blur" + e1.target.value)
+        if (aa == null || aa === 0) {
             setAAMandatory(1)
-            setNextButtonDisabled(1)
+            setNextButtonDisabled(true)
         }
         else {
             setAAMandatory(0)
-            setNextButtonDisabled(0)
+            setNextButtonDisabled(false)
         }
         console.log(isAAMandatory)
-        //checkErrors()
     }
 
     const handleAAChange = (e2) => {
-        console.log(e2)
         setAA(e2)
     }
 
     const handleCodeBlur = (e3) => {
         if (e3.target.value.length === 0 || e3.target.value === null) {
             setMandatory(1)
-            setNextButtonDisabled(1)
+            setNextButtonDisabled(true)
         }
         else {
-              setMandatory(0)
-              setNextButtonDisabled(0)
+            setMandatory(0)
+            setNextButtonDisabled(false)
         }
-        console.log(e3)
-        console.log(isCodeMandatory)
-        //checkErrors()
     }
 
     const handleCodeChange = (codeEvent) => {
-        console.log(codeEvent)
         setCode(codeEvent)
     }
 
-    const handleKindBlur = (kindBlurEvent) =>{
-        if(kindBlurEvent.target.value.length === 0 || kindBlurEvent.target.value === null) {
+    const handleKindBlur = (kindBlurEvent) => {
+        if (kindBlurEvent.target.value.length === 0 || kindBlurEvent.target.value === null) {
             setIsKindMandatory(1)
-            setNextButtonDisabled(1)
+            setNextButtonDisabled(true)
         }
         else {
             setIsKindMandatory(0)
-            setNextButtonDisabled(0)
+            setNextButtonDisabled(false)
         }
-        //checkErrors()
-    } 
+    }
 
     const handleKindChange = (kindEvent) => {
         setKind(kindEvent)
@@ -102,13 +106,12 @@ const MaterialTab = () => {
     const handleDescriptionBlur = (descblurEvent) => {
         if (descblurEvent.target.value.length === 0 || descblurEvent.target.value === null) {
             setDescriptionMandatory(1)
-            setNextButtonDisabled(1)
+            setNextButtonDisabled(true)
         }
         else {
             setDescriptionMandatory(0)
-            setNextButtonDisabled(0)
+            setNextButtonDisabled(false)
         }
-        //checkErrors()
     }
 
     const handleDescriptionChange = (descrChangeEvent) => {
@@ -118,16 +121,15 @@ const MaterialTab = () => {
     const handleQuantityBlur = (qblurEvent) => {
         if (qblurEvent.target.value.length === 0 || qblurEvent.target.value === null) {
             setQuantityMandatory(1)
-            setNextButtonDisabled(1)
+            setNextButtonDisabled(true)
         }
         else {
             setQuantityMandatory(0)
-            setNextButtonDisabled(0)
+            setNextButtonDisabled(false)
         }
-        //checkErrors()
     }
 
-    const handleQuantityChange =(qChangeEvent) => {
+    const handleQuantityChange = (qChangeEvent) => {
         setQuantity(qChangeEvent)
     }
 
@@ -137,9 +139,9 @@ const MaterialTab = () => {
 
     const handleNameChange = (nameEvent) => {
         setName(nameEvent)
-    } 
+    }
 
-    const handleRestDetailsChange = (detailsEvent)=> {
+    const handleRestDetailsChange = (detailsEvent) => {
         setRestDetails(detailsEvent)
     }
 
@@ -148,17 +150,18 @@ const MaterialTab = () => {
     }
 
     const handleSubmit = (submitEvent) => {
+
         console.log(submitEvent)
         console.log({
             aa: aa,
             code: code,
             kind: kind,
             description: description,
-            quantity:quantity,
-            name:name,
-            section:section,
-            restDetails:restDetails,
-            sector:section
+            quantity: quantity,
+            name: name,
+            section: section,
+            restDetails: restDetails,
+            sector: sector
         })
     }
 
@@ -166,26 +169,12 @@ const MaterialTab = () => {
      * Error messages
      */
     const errorString = 'Υποχρεωτικό πεδίο '
-    const aaErrorMessage = isAAMandatory==1 ? 'Πρέπει μεγαλύτερο απο 0' : null
-    const codeErrorMessage = isCodeMandatory==1 ? errorString : null;
-    const kindErrorMessage = isKindMandatory==1 ? errorString : null
-    const descrErrorMessage = isDescriptionMandatory==1 ? errorString : null
-    const quantityErrorMessage = isQuantityMandatory==1 ? errorString:null
+    const aaErrorMessage = isAAMandatory === 1 ? 'Πρέπει μεγαλύτερο απο 0' : null
+    const codeErrorMessage = isCodeMandatory === 1 ? errorString : null;
+    const kindErrorMessage = isKindMandatory === 1 ? errorString : null
+    const descrErrorMessage = isDescriptionMandatory === 1 ? errorString : null
+    const quantityErrorMessage = isQuantityMandatory === 1 ? errorString : null
 
-    const checkErrors = () => {
-
-        if (isAAMandatory ==1 || isCodeMandatory==1 || isKindMandatory==1 || isDescriptionMandatory==1 || isQuantityMandatory==1) {
-            //TO DO FIND BETTER WAY TO DISABLE BUTTON
-            isNextDisabled = true
-            //setNextButtonDisabled(1)
-        }
-        // else {
-        //     setNextButtonDisabled(0)
-        // }
-        console.log("Disabled : ")
-        console.log(isNextDisabled)
-        return isNextDisabled
-    }
 
     return (
 
@@ -205,6 +194,14 @@ const MaterialTab = () => {
                 <hr />
                 <FlexboxGrid justify="center">
                     <FlexboxGrid.Item colspan={12}>
+                        {!hasAllMandatoryFields && <Message
+                            showIcon
+                            type="error"
+                            title="Σφάλμα"
+                            closable={true}
+                            description="Συπληρώστε όλα τα απαραίτητα πεδία"
+                            onClose={resetError}                           
+                        />}
 
                         <Form layout="horizontal" onSubmit={handleSubmit}>
                             {step + 1 == 1 ?
@@ -212,7 +209,7 @@ const MaterialTab = () => {
                                     <FormGroup>
                                         <ControlLabel>Α/Α</ControlLabel>
                                         <ControlLabel style={mandatory}>*</ControlLabel>
-                                        <FormControl name="namea" type="number" onBlur={handleAABlur} onChange={handleAAChange} errorMessage={aaErrorMessage} placement="bottomStart"/>
+                                        <FormControl name="namea" type="number" onBlur={handleAABlur} onChange={handleAAChange} errorMessage={aaErrorMessage} placement="bottomStart" />
                                     </FormGroup>
                                     <FormGroup >
                                         <ControlLabel>Κωδικός</ControlLabel>
@@ -222,7 +219,7 @@ const MaterialTab = () => {
                                     <FormGroup>
                                         <ControlLabel>Είδος</ControlLabel>
                                         <ControlLabel style={mandatory}>*</ControlLabel>
-                                        <FormControl name="textarea" componentClass="textarea" onBlur={handleKindBlur} onChange={handleKindChange} errorMessage={kindErrorMessage} placement="bottomStart"/>
+                                        <FormControl name="textarea" componentClass="textarea" onBlur={handleKindBlur} onChange={handleKindChange} errorMessage={kindErrorMessage} placement="bottomStart" />
                                     </FormGroup>
                                     <FormGroup>
                                         <ControlLabel>Περιγραφή</ControlLabel>
@@ -236,7 +233,7 @@ const MaterialTab = () => {
                                     </FormGroup>
                                     <Row className="show-grid">
                                         <Col xs={6} xsPush={18}>
-                                        <Button onClick={onNext} onMouseEnter={checkErrors} disabled={isNextDisabled}>Επόμενο <Icon icon="page-next" /></Button>      </Col>
+                                            <Button onClick={onNext} disabled={isNextDisabled}>Επόμενο <Icon icon="page-next" /></Button>      </Col>
                                         <Col xs={6} xsPull={6}></Col>
                                     </Row>
 
@@ -247,15 +244,15 @@ const MaterialTab = () => {
                                     </FormGroup>
                                     <FormGroup>
                                         <ControlLabel>Ονοματεπώνυμο</ControlLabel>
-                                        <FormControl name="textarea" onChange={handleNameChange}/>
+                                        <FormControl name="textarea" onChange={handleNameChange} />
                                     </FormGroup>
                                     <FormGroup>
                                         <ControlLabel>Υπόλοιπα</ControlLabel>
-                                        <FormControl name="textarea" onChange={handleRestDetailsChange}/>
+                                        <FormControl name="textarea" onChange={handleRestDetailsChange} />
                                     </FormGroup>
                                     <FormGroup>
                                         <ControlLabel>Τομέας</ControlLabel>
-                                        <FormControl name="textarea" onChange={handleSectorChange}/>
+                                        <FormControl name="textarea" onChange={handleSectorChange} />
                                     </FormGroup>
                                     <FormGroup>
                                         <ButtonToolbar>
