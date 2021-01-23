767a7acdadf7aa1e1c786a6bea3629d7a2780dbe
P70299 <nikolaos.chantzopoulos@trasys.gr>
2021-01-23 02:48:10 +0200

    Shadowed panel and state management

--- src/views/employee/EmployeeStep1.js
+++ src/views/employee/EmployeeStep1.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Row, Grid, Col, FormControl, FormGroup, HelpBlock, ControlLabel, Icon, ButtonToolbar, Button, Form } from 'rsuite'
 
-const EmployeeStep1 = () => {
+const EmployeeStep1 = (props) => {
+
+    const {aa, code, name, expertise, handleChange, handleStep} = props
+    console.log(props)
 
     return (
         <Form fluid={true}>
@@ -11,26 +14,39 @@ const EmployeeStep1 = () => {
                     <Col xs={24} sm={12} md={8} lg={12}>
                         <FormGroup>
                             <ControlLabel>Α/Α</ControlLabel>
-                            <FormControl name="name" type="number" />
+                            <FormControl name="aa" type="number" value={aa} onChange={handleChange('aa')} />
                             <HelpBlock>Υποχρεωτικό</HelpBlock>
                         </FormGroup>
                         <FormGroup>
                             <ControlLabel>Κωδικός</ControlLabel>
-                            <FormControl name="code" />
+                            <FormControl name="code" value={code} onChange={handleChange('code')}/>
                             <HelpBlock>Υποχρεωτικό</HelpBlock>
                         </FormGroup>
                         <FormGroup>
                             <ControlLabel>Όνοματεπώνυμο</ControlLabel>
-                            <FormControl name="name" />
+                            <FormControl name="name" value={name} onChange={handleChange('name')}/>
                             <HelpBlock>Υποχρεωτικό</HelpBlock>
                         </FormGroup>
                         <FormGroup>
                             <ControlLabel>Ειδικότητα</ControlLabel>
-                            <FormControl name="expertise" />
+                            <FormControl name="expertise"  value={expertise} onChange={handleChange('expertise')}/>
                             <HelpBlock>Υποχρεωτικό</HelpBlock>
                         </FormGroup>
                     </Col>
                     <Col xs={24} sm={12} md={8} lg={6}></Col>
+                </Row>
+                <Row>
+                    <Col xs={24} sm={12} md={8} lg={6}></Col>
+                    <Col xs={24} sm={12} md={8} lg={6}></Col>
+                    <Col xs={24} sm={12} md={8} lg={6}>
+
+                    </Col>
+                </Row>
+                <Row className="show-grid">
+                    <Col md={4} mdOffset={20}>
+                    <ButtonToolbar>
+                            <Button appearance="primary" color="green" onClick={handleStep(2)}>Επόμενο<Icon icon="page-next"/></Button>
+                        </ButtonToolbar>                    </Col>
                 </Row>
             </Grid>
         </Form>
--- src/views/employee/EmployeeTab.js
+++ src/views/employee/EmployeeTab.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import {
   Content, Breadcrumb, Header, Steps,
   Panel
@@ -9,27 +10,65 @@ import EmployeeStep2 from './EmployeeStep2'
 const EmployeeTab = () => {
 
   const [step, setStep] = React.useState(1);
+  const [employeeState, setEmployeeState] = useState({
+    aa: 0,
+    code: '',
+    name: 'foo',
+    expertise: '',
+    property: ''
+  })
 
-  
-    return (
-      <Panel>
-        <Header>
-          <Breadcrumb>
-            <Breadcrumb.Item href="/">Αρχική</Breadcrumb.Item>
-            <Breadcrumb.Item href="/employeetab" active>Καρτέλα εργαζομένου</Breadcrumb.Item>
-          </Breadcrumb>
-        </Header>
-        <Content>
+
+  //will be later used for update purposes
+  // useEffect(()=>{
+  //   setEmployeeState({
+  //     aa:10,
+  //     code:'lwn',
+  //     name:'foo',
+  //     expertise:'none',
+  //     property:'property'
+  //   })
+  // },[])
+
+  const handleChange = (prop) => (evt) => {
+    console.log(evt)
+    console.log(prop)
+    setEmployeeState({ ...employeeState, [prop]: evt })
+  }
+
+  const handleStep = (stepValue) => (evn) => {
+    setStep(stepValue)
+  }
+
+
+
+
+
+  return (
+    <Panel>
+      <Header>
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Αρχική</Breadcrumb.Item>
+          <Breadcrumb.Item href="/employeetab" active>Καρτέλα εργαζομένου</Breadcrumb.Item>
+        </Breadcrumb>
+      </Header>
+      <Content>
+        <Panel shaded bordered>
+
           <Steps current={step}>
             <Steps.Item title="Βασικά στοιχεία" />
             <Steps.Item title="Παρατηρήσεις" />
           </Steps>
-          <hr />
-          <EmployeeStep1/>
-          <EmployeeStep2/>
-        </Content>
-      </Panel>
-    )
+        </Panel>
+        <hr />
+        <Panel shaded bordered>
+          {step == 1 ? <EmployeeStep1 {...employeeState}
+            handleChange={handleChange} handleStep={handleStep} /> :
+            <EmployeeStep2 {...employeeState} handleChange={handleChange} handleStep={handleStep} />}
+        </Panel>
+      </Content>
+    </Panel>
+  )
 
 }
 
